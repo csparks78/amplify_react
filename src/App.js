639d6b0f269e30c9adcc68634e45c2793b9e770f
Sparks <43890328+csparks78@users.noreply.github.com>
2023-01-14 15:47:30 -0700

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,148 @@
+// import React, { useState, useEffect } from "react";
+// import "./App.css";
+// import "@aws-amplify/ui-react/styles.css";
+// import { API, Storage } from 'aws-amplify';
+// import {
+//   Button,
+//   Flex,
+//   Heading,
+//   Image,
+//   Text,
+//   TextField,
+//   View,
+//   withAuthenticator,
+// } from "@aws-amplify/ui-react";
+
+// import { listTodos } from "./graphql/queries";
+// import {
+//   createTodo as createNoteMutation,
+//   deleteTodo as deleteNoteMutation,
+// } from "./graphql/mutations";
+
+
+
+
+// const App = ({ signOut }) => {
+//   const [notes, setTodo] = useState([]);
+
+//   useEffect(() => {
+//     fetchTodo();
+//   }, []);
+
+//   async function fetchTodo() {
+//     const apiData = await API.graphql({ query: listTodos });
+//     const notesFromAPI = apiData.data.listTodo.items;
+//     await Promise.all(
+//       notesFromAPI.map(async (note) => {
+//         if (note.image) {
+//           const url = await Storage.get(note.name);
+//           note.image = url;
+//         }
+//         return note;
+//       })
+//     );
+//     setTodo(notesFromAPI);
+//   }
+
+//   async function createTodo(event) {
+//     event.preventDefault();
+//     const form = new FormData(event.target);
+//     const image = form.get("image");
+//     const data = {
+//       name: form.get("name"),
+//       description: form.get("description"),
+//       image: image.name,
+//     };
+//     if (!!data.image) await Storage.put(data.name, image);
+//     await API.graphql({
+//       query: createNoteMutation,
+//       variables: { input: data },
+//     });
+//     fetchTodo();
+//     event.target.reset();
+//   }
+
+//   async function deleteTodo({ id, name }) {
+//     const newTodo = notes.filter((note) => note.id !== id);
+//     setTodo(newTodo);
+//     await Storage.remove(name);
+//     await API.graphql({
+//       query: deleteNoteMutation,
+//       variables: { input: { id } },
+//     });
+//   }
+
+//   return (
+//     <View className="App">
+//       <Heading level={1}>My Notes App</Heading>
+//       <View as="form" margin="3rem 0" onSubmit={createTodo}>
+//         <Flex direction="row" justifyContent="center">
+
+//           <TextField
+//             name="name"
+//             placeholder="Note Name"
+//             label="Note Name"
+//             labelHidden
+//             variation="quiet"
+//             required
+//           />
+//           <TextField
+//             name="description"
+//             placeholder="Note Description"
+//             label="Note Description"
+//             labelHidden
+//             variation="quiet"
+//             required
+//           />
+//           <View
+//             name="image"
+//             as="input"
+//             type="file"
+//             style={{ alignSelf: "end" }}
+//           />
+//           <Button type="submit" variation="primary">
+//             Create Note
+//           </Button>
+//         </Flex>
+//       </View>
+
+//       <Heading level={2}>Current Notes</Heading>
+//       <View margin="3rem 0">
+//         {notes.map((note) => (
+//           <Flex
+//             key={note.id || note.name}
+//             direction="row"
+//             justifyContent="center"
+//             alignItems="center"
+//           >
+//             <Text as="strong" fontWeight={700}>
+//               {note.name}
+//             </Text>
+//             <Text as="span">{note.description}</Text>
+//             {note.image && (
+//               <Image
+//                 src={note.image}
+//                 alt={`visual aid for ${notes.name}`}
+//                 style={{ width: 400 }}
+//               />
+//             )}
+//             <Button variation="link" onClick={() => deleteTodo(note)}>
+//               Delete note
+//             </Button>
+//           </Flex>
+//         ))}
+
+
+//       </View>
+//       <Button onClick={signOut}>Sign Out</Button>
+//     </View>
+//   );
+// };
+
+// export default withAuthenticator(App);
+
+////////////////////////////////////////////////////////////////////////////
+
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
@@ -11,68 +156,43 @@ import {
   View,
   withAuthenticator,
 } from "@aws-amplify/ui-react";
-import { listTodos } from "./graphql/queries";
+import { listNotes } from "./graphql/queries";
 import {
-  createTodo as createNoteMutation,
-  deleteTodo as deleteNoteMutation,
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
-import { API, Storage } from 'aws-amplify';
-import {
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from '@aws-amplify/ui-react';
-
 
 const App = ({ signOut }) => {
-  const [notes, setTodo] = useState([]);
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
-    fetchTodo();
+    fetchNotes();
   }, []);
 
-  async function fetchTodo() {
-    const apiData = await API.graphql({ query: listTodos });
-    const notesFromAPI = apiData.data.listTodos.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
-    setTodo(notesFromAPI);
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    setNotes(notesFromAPI);
   }
 
-  async function createTodo(event) {
+  async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
-    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
-      image: image.name,
     };
-    if (!!data.image) await Storage.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
     });
-    fetchTodo();
+    fetchNotes();
     event.target.reset();
   }
 
   async function deleteNote({ id }) {
     const newNotes = notes.filter((note) => note.id !== id);
-    setTodo(newNotes);
-    await Storage.remove(name);
+    setNotes(newNotes);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -82,7 +202,7 @@ const App = ({ signOut }) => {
   return (
     <View className="App">
       <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createTodo}>
+      <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
             name="name"
@@ -118,35 +238,6 @@ const App = ({ signOut }) => {
               {note.name}
             </Text>
             <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
-        <View
-          name="image"
-          as="input"
-          type="file"
-          style={{ alignSelf: "end" }}
-        />
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            {note.image && (
-              <Image
-                src={note.image}
-                alt={`visual aid for ${notes.name}`}
-                style={{ width: 400 }}
-              />
-            )}
             <Button variation="link" onClick={() => deleteNote(note)}>
               Delete note
             </Button>
